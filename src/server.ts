6f3afe3a7b7f7c8d4ca2bd2c61d6543213bf87e7
Andriy Shymkiv <winamondezik@gmail.com>
2023-01-05 22:36:42 +0200

    test

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+
+import express from 'express';
+import cors from 'cors';
+import serverless from 'serverless-http';
+import { router as productRouter } from './routes/products';
+
+const router = express.Router();
+const app = express();
+const API_PATH = '/.netlify/functions/server';
+
+app.use(cors());
+
+app.use(`${API_PATH}/products`, productRouter);
+app.use(API_PATH, router);
+
+export const handler = serverless(app);
+
+// for local testing
+
+const PORT = 5000 || null;
+
+app.listen(PORT, () => {
+  console.log(`server started at port ${PORT}`);
+});
